b89ec595085a279f740a07ef9a0a03c672590908
Vladimir Ignatenko <redcatdeveloper@gmail.com>
2023-06-28 00:28:04 +0300

    Added simple parser for INI file

--- goconf.go
+++ goconf.go
@@ -1,12 +1,65 @@
 package goconf
 
+import (
+  "os"
+  "bufio"
+  "strings"
+  "log"
+)
+
+type inidata map[string]string
+
 type goConf struct {
   Filename string `config.ini`
+  data inidata
 }
 
-func newGoConf(filename string) *goConf {
+func NewGoConf(filename string) (*goConf, error) {
   conf := &goConf {
     Filename: filename,
+    data: inidata{},
   }
-  return conf
+  _, err := conf.parseFile()
+  if err != nil {
+    return nil, err
+  }
+  return conf, nil
+}
+
+func (conf *goConf) Get(param string) string {
+  value, ok := conf.data[param]
+  if ok {
+    return value
+  }
+  return ""
+}
+
+func (conf *goConf) parseFile() (bool, error) {
+  f, err := os.OpenFile(conf.Filename, os.O_RDONLY, 0666)
+  if err != nil {
+    log.Print(err)
+    return false, err
+  }
+  defer f.Close()
+
+  r := bufio.NewReader(f)
+
+  for {
+    line, err := r.ReadString('\n')
+    if err != nil {
+      break
+    }
+    conf.parseLine(line)
+  }
+  return true, nil
+}
+
+func (conf *goConf) parseLine(line string) {
+  parts := strings.Split(strings.Trim(line, "\r\n\t "), "=")
+  if len(parts) == 2 {
+    key, value := strings.Trim(parts[0], "\r\n\t "), strings.Trim(parts[1], "\r\n\t ")
+    if key != "" {
+      conf.data[key] = value
+    }
+  }
 }
