452ea191bd8154370db389317bb8a124294c1c7e
Vladimir Ignatenko <redcatdeveloper@gmail.com>
2024-01-31 00:02:42 +0300

    Added test

---
+++ goconf_test.go
@@ -0,0 +1,17 @@
+package goconf
+
+import (
+	"testing"
+)
+
+func TestSplitLine(t *testing.T) {
+	conf := &goConf{}
+	str := "Test = 12"
+	key, value := conf.splitLine(str)
+	if key != "Test" {
+		t.Errorf("Key got \"%s\" want \"%s\"", key, "Test")
+	}
+	if value != "12" {
+		t.Errorf("Value got \"%s\" want \"%s\"", value, "12")
+	}
+}
