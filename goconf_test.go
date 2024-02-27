01887da82ad8da9576a387f0d2dc16801906f4a8
Vladimir Ignatenko <redcatdeveloper@gmail.com>
2024-02-27 23:42:59 +0300

    Add tests

--- goconf_test.go
+++ goconf_test.go
@@ -4,6 +4,19 @@ import (
 	"testing"
 )
 
+func TestParseFile(t *testing.T) {
+	conf := NewGoConf()
+	status, err := conf.parseFile("test/config1.ini")
+	if status {
+		value := conf.Get("test")
+		if value != "Hello World" {
+			t.Errorf("Value got \"%s\" want \"%s\"", value, "Hello World")
+		}
+	} else {
+		t.Errorf("Error read file: %s", err)
+	}
+}
+
 func TestSplitLine(t *testing.T) {
 	conf := NewGoConf()
 	str := "Test = 12"
